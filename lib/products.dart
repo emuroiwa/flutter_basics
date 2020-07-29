35ec6665d8a934028504466fa89ece3f8bf879e0
Ernest <emuroiwa@gmail.com>
2020-07-29 11:59:49 +0200

    master: WIP products file

---
+++ lib/products.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+  Products(this.products);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+              children: products
+                  .map((e) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(e)
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+    );
+  }
+} {}
